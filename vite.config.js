e9412dea64bffe1eca0519fba95d9a7007fb755d
azamat <abdusamadovazamat46@gmail.com>
2025-05-22 19:11:52 +0500

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/todo-react/" : "/"
+  base: process.env.NODE_ENV === "production" ? "/TodoReact_PRODUCTION/" : "/"
 })
